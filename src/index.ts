fad3f7f6742d1bdc67291cf0d059154621f0157c
Alex Dennis <alex.dennis@gmail.com>
2023-09-30 16:45:02 -0700

    Talking to the Baudelaire orphans

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,47 @@ import {
   BedrockRuntimeClient,
   InvokeModelCommand,
 } from "@aws-sdk/client-bedrock-runtime";
+import { AI21ModelRequest, AI21ModelResponse } from "./types";
 
 const client = new BedrockRuntimeClient({ region: "us-east-1" });
-const fn = async () => {};
+
+const PROMPT = `
+System: You are a chatbot designed to give users the ability to talk to characters in any book. Play the role of {character} in the {book}.  
+Don't break out of character. Please give chapter and verse numbers that validate your answers where possible. Don't refer to yourself in the third person.
+Respond in less than 500 words and avoid repetition. Only answer questions that you are asked.
+
+Question: 
+`;
+
+const fn = async () => {
+  const modelRequest: AI21ModelRequest = {
+    prompt:
+      PROMPT.replace("{character}", "Violet").replace(
+        "{book}",
+        "A series of unfortunate events"
+      ) + "Who were your siblings and what is special about each of them?",
+    maxTokens: 200,
+    temperature: 0,
+    topP: 1.0,
+    stopSequences: [],
+    countPenalty: { scale: 0 },
+    presencePenalty: { scale: 0 },
+    frequencyPenalty: { scale: 0 },
+  };
+
+  const command = new InvokeModelCommand({
+    modelId: "ai21.j2-ultra-v1",
+    contentType: "application/json",
+    accept: "*/*",
+    body: JSON.stringify(modelRequest),
+  });
+
+  const output = await client.send(command);
+  const modelResponse: AI21ModelResponse = JSON.parse(
+    Buffer.from(output.body).toString()
+  );
+
+  console.log(JSON.stringify(modelResponse.completions[0].data.text, null, 2));
+};
 
 fn();
---
+++ src/types/ai21.ts
@@ -0,0 +1,81 @@
+export type AI21ModelRequestPenalty = {
+  scale: number;
+  applyToNumbers?: boolean;
+};
+
+export type AI21ModelRequest = {
+  /**
+   * This is the starting point for generating responses. The format of the prompt, whether zero-shot, few-shot, or instructional, can influence the shape of the model's responses.
+   */
+  prompt: string;
+  /**
+   * The maximum number of tokens to generate for each response.
+   */
+  maxTokens: number;
+  /**
+   * A value controlling the "creativity" of the model's responses.
+   */
+  temperature: number;
+  /**
+   * A value controlling the diversity of the model's responses.
+   */
+  topP: number;
+  /**
+   * A list of sequences that, when generated, will cause the model to stop generating tokens.
+   */
+  stopSequences: string[];
+
+  /**
+   * A penalty applied to tokens based on their frequency in the generated responses.
+   */
+  countPenalty: AI21ModelRequestPenalty;
+
+  /**
+   * A penalty applied to tokens that are frequently generated.
+   */
+  frequencyPenalty: AI21ModelRequestPenalty;
+
+  /**
+   * A penalty applied to tokens that are already present in the prompt.
+   */
+  presencePenalty: AI21ModelRequestPenalty;
+};
+
+export type AI21ModelResponseTopToken = {
+  token: string;
+  logprob: number;
+};
+
+export type AI21ModelResponseTokenData = {
+  generatedToken: {
+    token: string;
+    logprob: number;
+    raw_logprob: number;
+  };
+  topTokens?: AI21ModelResponseTopToken[];
+  textRange: {
+    start: number;
+    end: number;
+  };
+};
+
+export type AI21ModelResponseCompletion = {
+  data: {
+    text: string;
+    tokens: AI21ModelResponseTokenData[];
+  };
+  finishReason: {
+    reason: "length" | "endoftext" | "stop";
+    length?: number;
+    sequence?: string;
+  };
+};
+
+export type AI21ModelResponse = {
+  id: string;
+  prompt: {
+    text: string;
+    tokens: AI21ModelResponseTokenData[];
+  };
+  completions: AI21ModelResponseCompletion[];
+};
